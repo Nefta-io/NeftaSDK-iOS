e7ec0b1138feff7fd8868e11361015203ea18f50
Tomaz Treven <treven@nefta.io>
2024-01-10 16:23:07 +0100

    Added option to copy appId and Nuid to example integration

--- DirectIntegration_iOS/DirecIntegration_iOS/ViewController.h
+++ DirectIntegration_iOS/DirecIntegration_iOS/ViewController.h
@@ -10,8 +10,11 @@
 
 @interface ViewController : UIViewController
 @property NeftaPlugin_iOS *plugin;
+@property NSString *appId;
+@property NSString *nuid;
 @property (weak, nonatomic) IBOutlet UIView *placementContainer;
 @property (weak, nonatomic) IBOutlet UILabel *appIdLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nuidLabel;
 @property NSMutableDictionary* controllers;
 @end
 
--- DirectIntegration_iOS/DirecIntegration_iOS/ViewController.m
+++ DirectIntegration_iOS/DirecIntegration_iOS/ViewController.m
@@ -18,13 +18,28 @@
 
     _controllers = [[NSMutableDictionary alloc] init];
 
-    NSString *appId = @"5070114386870272";
+    _appId = @"5630785994358784";
     [NeftaPlugin_iOS EnableLogging: true];
-    _plugin = [NeftaPlugin_iOS InitWithAppId: appId];
+    _plugin = [NeftaPlugin_iOS InitWithAppId: _appId];
     
     __unsafe_unretained typeof(self) weakSelf = self;
     
     _plugin.OnReady = ^(NSDictionary<NSString *, Placement *> * placements) {
+        NSString* userString = [weakSelf->_plugin GetToolboxUser];
+        NSData *userData = [userString dataUsingEncoding:NSUTF8StringEncoding];
+        if (userData) {
+            NSError *error = nil;
+            id user = [NSJSONSerialization JSONObjectWithData:userData options:0 error:&error];
+            if (error) {
+                NSLog(@"Error parsing user json: %@", error.localizedDescription);
+            } else {
+                if ([user isKindOfClass:[NSDictionary class]]) {
+                    weakSelf->_nuid = user[@"user_id"];
+                    [weakSelf->_nuidLabel setText: weakSelf->_nuid];
+                }
+            }
+        }
+        
         int i = 0;
         for (NSString* placementId in placements) {
             PlacementUiView *controller = (PlacementUiView *)[[NSBundle mainBundle] loadNibNamed:@"PlacementUiView" owner:nil options:nil][0];
@@ -63,12 +78,41 @@
     [_plugin EnableAds: true];
     
     NSString *appIdLabel;
-    if (appId == nil || [appId length] == 0) {
+    if (_appId == nil || [_appId length] == 0) {
         appIdLabel = @"Demo mode (appId not set)";
     } else {
-        appIdLabel = [NSString stringWithFormat: @"AppId: %@", appId];
+        appIdLabel = [NSString stringWithFormat: @"AppId: %@", _appId];
     }
     [_appIdLabel setText: appIdLabel];
+    UITapGestureRecognizer *appIdGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(appIdTapped:)];
+    [_appIdLabel addGestureRecognizer: appIdGesture];
+    [_appIdLabel setUserInteractionEnabled: YES];
+    
+    UITapGestureRecognizer *nuidGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(nuidTapped:)];
+    [_nuidLabel addGestureRecognizer: nuidGesture];
+    [_nuidLabel setUserInteractionEnabled: YES];
+}
+
+- (void)appIdTapped:(UITapGestureRecognizer *)gestureRecognizer {
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    [pasteboard setString: _appId];
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"AppId copied"
+                                                                   message:_appId
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)nuidTapped:(UITapGestureRecognizer *)gestureRecognizer {
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    [pasteboard setString: _nuid];
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Nuid copied"
+                                                                   message:_nuid
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
